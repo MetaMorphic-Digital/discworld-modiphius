03a9f659636b0cf7fd0a79069a22d40da856d650
jalensailin <jalen.michalslevy@gmail.com>
2024-12-17 17:53:57 -0500

    initial actor data model

---
+++ modules/datamodels/character-schema.js
@@ -0,0 +1,24 @@
+export default class CharacterDataModel extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    const { fields } = foundry.data;
+
+    return {
+      description: new fields.StringField({ required: true }),
+      luck: new fields.SchemaField({
+        max: new fields.NumberField({
+          required: true,
+          nullable: false,
+          positive: true,
+          integer: true,
+          initial: 4,
+        }),
+        value: new fields.NumberField({
+          required: true,
+          nullable: false,
+          integer: true,
+          initial: 4,
+        }),
+      }),
+    };
+  }
+}
--- modules/discworld.js
+++ modules/discworld.js
@@ -1,3 +1,9 @@
-Hooks.on("ready", () => {
-  console.log("Discworld Loaded");
+import CharacterDataModel from "./datamodels/character-schema.js";
+
+Hooks.on("init", () => {
+  // Register Actor data models.
+  CONFIG.Actor.dataModels.character = CharacterDataModel;
+
+  // Register Item data models.
+  // CONFIG.Item.dataModels.trait = TraitDataModel;
 });
