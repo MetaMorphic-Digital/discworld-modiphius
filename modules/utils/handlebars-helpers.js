71f7e7f277380686efacedf79162b6bd41e51f56
jalensailin <jalen.michalslevy@gmail.com>
2024-12-18 13:58:44 -0500

    add `compare` hbs helper

--- modules/utils/handlebars-helpers.js
+++ modules/utils/handlebars-helpers.js
@@ -1,6 +1,74 @@
 import DISCWORLD from "../config.js";
 
 export default function registerHelpers() {
+  /**
+   * Compares two values with the given operator. If no operator is provided,
+   * '===' is used by default.
+   *
+   * @see http://doginthehat.com.au/2012/02/comparison-block-helper-for-handlebars-templates/#comment-44
+   * @param {*} lValue
+   * @param {string} operator
+   * @param {*} rValue
+   */
+  Handlebars.registerHelper("compare", function (...args) {
+    if (args.length < 3) {
+      throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+    }
+
+    // eslint-disable-next-line prefer-const
+    let [lValue, operator, rValue, options] = args;
+
+    if (options === undefined) {
+      options = rValue;
+      rValue = operator;
+      operator = "===";
+    }
+
+    const operators = {
+      "===": (l, r) => {
+        return l === r;
+      },
+      "!==": (l, r) => {
+        return l !== r;
+      },
+      "<": (l, r) => {
+        return l < r;
+      },
+      ">": (l, r) => {
+        return l > r;
+      },
+      "<=": (l, r) => {
+        return l <= r;
+      },
+      ">=": (l, r) => {
+        return l >= r;
+      },
+      typeof(l, r) {
+        // eslint-disable-next-line valid-typeof
+        return typeof l === r;
+      },
+      "&&": (l, r) => {
+        return !!l && !!r;
+      },
+      "||": (l, r) => {
+        return !!l || !!r;
+      },
+    };
+
+    if (!operators[operator]) {
+      throw new Error(
+        `Handlerbars Helper 'compare' doesn't know the operator ${operator}`,
+      );
+    }
+
+    const result = operators[operator](lValue, rValue);
+
+    if (result) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
+
   /* ****************
    * CONFIG HELPERS *
    **************** */
