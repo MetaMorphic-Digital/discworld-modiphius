2ef1c6b4667a4343142c9ab23773bfc784438733
jalensailin <jalen.michalslevy@gmail.com>
2025-01-20 15:11:51 -0500

    Add global `discworld` property

--- modules/discworld.js
+++ modules/discworld.js
@@ -9,8 +9,22 @@ import DiscworldRoll from "./rolls/rolls.js";
 import DISCWORLD from "./config.js";
 import DiscworldMessage from "./chat/chat-message.js";
 import DiscworldCharacter from "./documents/character.js";
+import transitionClass from "./utils/animations.js";
+
+// Export globals.
+globalThis.discworld = {
+  config: DISCWORLD,
+  utils: {
+    transitionClass,
+    registerHelpers,
+    registerKeybindings,
+  },
+};
 
 Hooks.once("init", () => {
+  // Configuration.
+  CONFIG.Discworld = DISCWORLD;
+
   // Register Actor classes.
   CONFIG.Actor.documentClass = DiscworldCharacter;
   CONFIG.Actor.dataModels.character = CharacterDataModel;
