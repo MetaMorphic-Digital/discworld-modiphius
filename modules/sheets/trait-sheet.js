b3a6dc839ed5d4c75c5275caf1473e3accea56b0
jalensailin <jalen.michalslevy@gmail.com>
2025-02-22 22:35:00 -0500

    update trait sheet to use default styling

--- modules/sheets/trait-sheet.js
+++ modules/sheets/trait-sheet.js
@@ -14,6 +14,9 @@ export default class TraitSheet extends DiscworldSheetMixin(ItemSheetV2) {
   };
 
   static PARTS = {
+    decoration: {
+      template: "systems/discworld/templates/mixins/decoration.hbs",
+    },
     main: {
       template: "systems/discworld/templates/trait-sheet.hbs",
     },
