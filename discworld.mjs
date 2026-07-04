96c4b7f05c0d84d7deb43215ad08cf9db44cafc9
jalensailin <jalen.michalslevy@gmail.com>
2026-07-04 16:45:01 -0400

    Reorganize main imports

--- discworld.mjs
+++ discworld.mjs
@@ -1,15 +1,22 @@
-import CharacterDataModel from "./modules/datamodels/character-schema.mjs";
-import DiscworldChatLog from "./modules/chat/chat.mjs";
-import TraitDataModel from "./modules/datamodels/trait-schema.mjs";
-import TraitSheet from "./modules/applications/sheets/trait-sheet.mjs";
+import DISCWORLD from "./modules/config.mjs";
+
+import * as utils from "./modules/utils/_module.mjs";
 import preloadTemplates, { registerHelpers } from "./modules/utils/handlebars.mjs";
 import registerKeybindings from "./modules/utils/keybindings.mjs";
-import * as Rolls from "./modules/rolls/index.mjs";
-import DISCWORLD from "./modules/config.mjs";
+
+import CharacterDataModel from "./modules/datamodels/character-schema.mjs";
+import TraitDataModel from "./modules/datamodels/trait-schema.mjs";
+
+import DiscworldChatLog from "./modules/chat/chat.mjs";
 import DiscworldMessage from "./modules/chat/chat-message.mjs";
+
+import * as Rolls from "./modules/rolls/index.mjs";
+
 import DiscworldCharacter from "./modules/documents/character.mjs";
+
+import TraitSheet from "./modules/applications/sheets/trait-sheet.mjs";
 import DiscworldJournalEntrySheet from "./modules/applications/sheets/journal-entry-sheet.mjs";
-import * as utils from "./modules/utils/_module.mjs";
+
 import DiscworldActorSheet from "./modules/applications/sheets/actors/base-actor-sheet.mjs";
 import CharacterSheet from "./modules/applications/sheets/actors/character-sheet.mjs";
 
