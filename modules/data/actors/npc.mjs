04c03fa518090a09d4d36ff83422071edb73382b
jalensailin <jalen.michalslevy@gmail.com>
2026-07-22 13:10:32 -0400

    Add NPC Embeds

--- modules/data/actors/npc.mjs
+++ modules/data/actors/npc.mjs
@@ -1,3 +1,5 @@
+import { templatePath } from "../../utils/paths.mjs";
+
 const { HTMLField, StringField } = foundry.data.fields;
 
 export default class NPCData extends foundry.abstract.TypeDataModel {
@@ -18,4 +20,36 @@ export default class NPCData extends foundry.abstract.TypeDataModel {
     "DISCWORLD.character",
     "DISCWORLD.npc",
   ];
+
+  /**
+   * Organize traits into an object indexed by trait type.
+   * @type {Record<keyof typeof DISCWORLD.traitTypes.npc, Item[]>}
+   */
+  get traits() {
+    return Object.keys(discworld.config.traitTypes.npc).reduce((acc, traitType) => {
+      acc[traitType] = this.parent.items.filter((i) => i.system.type === traitType);
+      return acc;
+    }, {});
+  }
+
+  /** @inheritdoc */
+  async toEmbed(config, options) {
+    config.inline = true;
+
+    const embed = discworld.utils.createElement("aside", { classes: ["discworld-npc"] });
+    embed.innerHTML = await foundry.applications.handlebars.renderTemplate(
+      templatePath("embeds/npc.hbs"),
+      {
+        document: this.parent,
+        description: await CONFIG.ux.TextEditor.enrichHTML(this.description, options),
+        storyPrompt: await CONFIG.ux.TextEditor.enrichHTML(this.storyPrompt, options),
+        img: {
+          altText: config.alt ?? this.parent.name,
+          src: this.parent.img,
+          hide: config.hideImage,
+        },
+      },
+    );
+    return embed;
+  }
 }
